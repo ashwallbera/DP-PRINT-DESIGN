8452151a87949420552076dd36de3311b4bb6f92
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2021-12-12 17:51:54 +0800

    [ADD] add angular material

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
