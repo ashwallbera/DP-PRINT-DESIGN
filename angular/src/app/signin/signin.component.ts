8755c8a5b988418e15c23368c60fc9dcbcf04680
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2021-12-12 20:53:31 +0800

    [ADD] Add background image and form control for sign in

--- angular/src/app/signin/signin.component.ts
+++ angular/src/app/signin/signin.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  addProductForm: FormGroup;
+  constructor() {
+    this.addProductForm = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      
+    });
   }
 
+  ngOnInit(): void {}
+
+  getValues(val: any) {
+    console.log(val);
+  }
+
+  public checkError = (controlName: string, errorName: string)=>{
+    return this.addProductForm.controls[controlName].hasError(errorName);
+  }
 }
