78ed9479353a2836de25118c5beae1ccd86aa1b1
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2021-12-18 19:09:47 +0800

    [MOD] Add validation form

--- angular/src/app/signup/signup.component.ts
+++ angular/src/app/signup/signup.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators,FormControl} from '@angular/forms';
-
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from '../services/_login/login.service';
+import { LoginModel } from '../services/_login/login_model';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
   hide = true;
@@ -13,12 +20,20 @@ export class SignupComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   signupForms: FormGroup;
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(
+    private _formBuilder: FormBuilder,
+    public login_service: LoginService,
+    private _router: Router
+  ) {}
 
   ngOnInit() {
     this.signupForms = new FormGroup({
-       username: new FormControl('', [Validators.required]),
-      // password: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      firstname: new FormControl('', [Validators.required]),
+      lastname: new FormControl('', [Validators.required]),
+      role: new FormControl('', [Validators.required]),
+      
     });
 
     // this.firstFormGroup = this._formBuilder.group({
@@ -29,4 +44,11 @@ export class SignupComponent implements OnInit {
     // });
   }
 
+  getValues(val: LoginModel) {
+    //this.addProductForm.get("username")?.value // get the specific form
+    console.log(val);
+    this.login_service.createuser(val);
+    this._router.navigate(['/signin'])
+
+  }
 }
