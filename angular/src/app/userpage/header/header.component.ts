7c8610e19a1f331fede267463ba93e4ea8924d65
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2021-12-18 19:10:49 +0800

    [MOD] Add logout functionality

--- angular/src/app/userpage/header/header.component.ts
+++ angular/src/app/userpage/header/header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit, EventEmitter,Output} from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginModel } from 'src/app/services/_login/login_model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   @Output() toggleSidebarForMe: EventEmitter<any> = new EventEmitter();
 
-  constructor(private router: Router) {}
+  users: LoginModel[];
+  user: LoginModel;
+
+  constructor(private router: Router) {
+    this.users = JSON.parse('' + localStorage.getItem('user'));
+    this.user = this.users[0];
+  }
 
   ngOnInit(): void {}
 
@@ -17,9 +24,8 @@ export class HeaderComponent implements OnInit {
     this.toggleSidebarForMe.emit();
   }
 
-  logout(){
-    localStorage.clear()
-    this.router.navigate(['/'])
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/']);
   }
-
 }
