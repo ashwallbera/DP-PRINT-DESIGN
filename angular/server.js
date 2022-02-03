e8bbc7b2c368b11d5460f0a82c21ad30cde2abe3
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-02-04 01:56:43 +0800

    Initial Configuration for heroku

--- angular/server.js
+++ angular/server.js
@@ -1,5 +1,15 @@
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-  });
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angular'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
