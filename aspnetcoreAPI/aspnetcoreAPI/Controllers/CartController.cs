55559dd2d1593f38bb592f8e0b71494a5f8bf375
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-05-14 01:10:54 +0800

    [ADD] Initial Code for Cart controller

---
+++ aspnetcoreAPI/aspnetcoreAPI/Controllers/CartController.cs
@@ -0,0 +1,66 @@
+﻿using aspnetcoreAPI.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace aspnetcoreAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private string connectionString = "Server=.;Initial Catalog=DpPrintDesign;Integrated Security=true";
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CartModel>>> getCart(Guid userid)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string cmd = "SELECT * FROM cart where isDeleted='"+false+"' and customerid='"+userid+"'";
+                    var results = await connection.QueryAsync<CartModel>(cmd);
+
+                    var cart = results.ToList();
+                    connection.Close();
+                    if (results.ToList().Count > 0)
+                    {
+                        
+                        
+                    }
+                    return Ok(cart);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return BadRequest(ex);
+            }
+
+        }
+        [HttpPost]
+        public async Task<ActionResult> addCart(CartModel model)
+        {
+            try
+            {
+                model.isDeleted = false;
+                model.Id = Guid.NewGuid();
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string cmd = "INSERT INTO cart(id,productid,specification,price,created,isDeleted,customerid) values('"+model.Id+"','"+model.productId + "','"+model.specification+"','"+model.price+"','"+model.created+"','"+model.isDeleted+"','"+model.customerid+"') ";
+                    var results = await connection.QueryAsync<CartModel>(cmd);
+                    connection.Close();
+                }
+                    return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+    }
+}
---
+++ aspnetcoreAPI/aspnetcoreAPI/Models/CartModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+
+namespace aspnetcoreAPI.Models
+{
+    [DataContract]
+    public class CartModel
+    {
+        [DataMember(IsRequired =false)]
+        public Guid Id { get; set; }
+
+        [DataMember]
+        public Guid customerid { get; set; }
+
+        [DataMember]
+        public Guid productId { get; set; }
+
+        [DataMember]
+        public string specification { get; set; }
+
+        [DataMember(IsRequired = false)]
+        public string price { get; set; }
+
+        [DataMember(IsRequired =false)]
+        public string created { get; set; }
+
+        [DataMember(IsRequired = false)]
+        public List<ProductModel> product { get; set; }
+
+        [DataMember(IsRequired =false)]
+        public bool isDeleted { get; set; }
+    }
+}
