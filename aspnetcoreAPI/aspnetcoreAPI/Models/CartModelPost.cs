18fc7140d1a40e0a4e0160958962f1a6f9e70b96
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-05-21 23:19:02 +0800

    [ADD] Resources and Cart Post

---
+++ aspnetcoreAPI/aspnetcoreAPI/Models/CartModelPost.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization;
+
+namespace aspnetcoreAPI.Models
+{
+    [DataContract]
+    public class CartModelPost
+    {
+        [DataMember(IsRequired = false)]
+        public Guid Id { get; set; }
+
+        [DataMember]
+        public Guid customerid { get; set; }
+
+        [DataMember]
+        public Guid productId { get; set; }
+
+        [DataMember]
+        public string specification { get; set; }
+
+        [DataMember(IsRequired = false)]
+        public string price { get; set; }
+
+        [DataMember(IsRequired = false)]
+        public string created { get; set; }
+
+        [DataMember(IsRequired = false)]
+        public int qty { get; set; }
+
+
+        [DataMember(IsRequired = false)]
+        public bool isDeleted { get; set; }
+    }
+}
